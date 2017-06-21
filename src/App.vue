3373b97c693c4965ab123ff6b9733fdb0d320f13
Wendler Eis <wendler_eis@hotmail.com>
2017-06-20 21:43:09 -0300

    Added: App component

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>{{ message }}</h3>
+    <input v-model="message" />
+  </div>
+</template>
+
+<script>
+  module.exports = {
+    data: () => {
+      return {
+        message: 'Segura o bind'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
